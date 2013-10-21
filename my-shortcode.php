62229b9b9db9248a8ad4c7b43afa18fe8bb85d68
三英電業株式会社　情報システム <jyoho@sanei-dengyo.com>
2013-10-21 10:23:45 +0900

    ok

--- my-shortcode.php
+++ my-shortcode.php
@@ -2,4 +2,35 @@
 /**
  * Additional Shortcodes
  */
+//******************************************************************************
+// WikiUp Tooltip
+//******************************************************************************
+function sc_wikiup($atts, $content = null) {
+	extract(shortcode_atts(array(
+			'id' => '',
+			'style' => '',
+			'wiki' => '',
+			'lang' => 'ja',
+	), $atts));
+	// idを追加
+	$dataid = '';
+	if ($id != '') {
+		$dataid = ' id="'.$id.'"';
+	}
+	// styleを追加
+	$datastyle = '';
+	if ($style != '') {
+		$datastyle = ' style="'.$style.'"';
+	}
+	$ret  = '';
+	$ret .= '<data';
+	$ret .= $dataid;
+	$ret .= $datastyle;
+	$ret .= ' data-wiki="'.$wiki.'"';
+	$ret .= ' data-lang="'.$lang.'">';
+	$ret .= do_shortcode($content);
+	$ret .= '</data>';
+	return $ret;
+}
+add_shortcode('wikiup', 'sc_wikiup');
 ?>
